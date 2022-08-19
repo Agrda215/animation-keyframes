38dec6064367b326d86a1ac40105cc6a9c592ee4
Glitch (agreeable-bright-dracorex) <none>
2022-08-19 15:53:03 +0000

    update log, warn and error.

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
+var i = 0
+var c = ""
 
-console.log("Hello, world!");
+console.log("%cHello, world!","font-size:30px;font-family:Droid Sans;");
+
+console.log(
+  `%cSOURCES
+%c<link %crel=%c"stylesheet" %chref=%c"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"%c />
+%cFONT AWESOME 4.7.0!`,
+  "font-size:23px;",
+  "font-size:18px;color:orange;",
+  "font-size:18px;color:yellow;",
+  "font-size:18px;color:darkgreen;",
+  "font-size:18px;color:yellow;",
+  "font-size:18px;color:darkgreen;",
+  "font-size:18px;color:orange;",
+  "font-size:20px;text-shadow:2px 2px 4px yellow;color:yellow;",
+)
+
+while (i < 4444) {
+  c += String(new Date().getFullYear())
+  i += 1
+}
+
+console.log(c)
+console.warn(c)
+console.error(c)
+console.log(c.length)
